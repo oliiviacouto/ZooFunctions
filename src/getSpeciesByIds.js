4c5e89e527a0e8887f8b36a938549fa18d13bded
Olivia Couto <olivia.acouto@gmail.com>
2022-11-02 12:36:20 -0300

    resolução 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const animaisId = data.species.filter((id) => ids.includes(id.id));
+  return animaisId;
 }
-
 module.exports = getSpeciesByIds;
