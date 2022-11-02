9a79aee4be4fd0117c925e129c69447a238c6007
Olivia Couto <olivia.acouto@gmail.com>
2022-11-02 14:43:50 -0300

    resolução 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAnimals = (animal) => {
+  const leozinho = {};
+  if (!animal) {
+    species.forEach((specie) => {
+      leozinho[specie.name] = specie.residents.length;
+    });
+    return leozinho;
+  }
+  if (!animal.sex) {
+    return species.find((specie) => specie.name === animal.specie)
+      .residents.length;
+  }
+  return species.find((specie) => specie.name === animal.specie).residents
+    .filter((tica) => tica.sex === animal.sex).length;
+};
 
 module.exports = countAnimals;
