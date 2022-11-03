c6156a66c65eccab7971b2c23d2bdf86f0659081
Olivia Couto <olivia.acouto@gmail.com>
2022-11-02 22:52:30 -0300

    resolução 9 e melhora resolução 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,9 @@
 const { prices } = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  const child = entrants.filter((obj1) => obj1.age > 0 && obj1.age < 18).length;
-  const adult = entrants.filter((obj2) => obj2.age >= 18 && obj2.age < 50).length;
-  const senior = entrants.filter((obj3) => obj3.age >= 50).length;
+  const child = entrants.filter((obj1) => obj1.age > 0 && obj1.age < 18).length || 0;
+  const adult = entrants.filter((obj2) => obj2.age >= 18 && obj2.age < 50).length || 0;
+  const senior = entrants.filter((obj3) => obj3.age >= 50).length || 0;
   return { child, adult, senior };
 };
 
@@ -11,11 +11,16 @@ const calculateEntry = (entrants) => {
   if (!entrants || Object.keys(entrants).length === 0) {
     return 0;
   }
-  const visitantes = Object.values(countEntrants(entrants));
-  const leozinho = visitantes[0] * prices.child;
-  const tica = visitantes[1] * prices.adult;
-  const helena = visitantes[2] * prices.senior;
-  const total = leozinho + tica + helena;
+
+  const { child, adult, senior } = countEntrants(entrants);
+
+  const total = child * prices.child + adult * prices.adult + senior * prices.senior;
+
+  // const visitantes = Object.values(countEntrants(entrants));
+  // const leozinho = visitantes[0] * prices.child;
+  // const tica = visitantes[1] * prices.adult;
+  // const helena = visitantes[2] * prices.senior;
+  // const total = leozinho + tica + helena;
   return total;
 };
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,61 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const daysOfWeek = Object.keys(hours);
+const names = species.map((specie) => specie.name);
+
+function isName(name) {
+  return names.includes(name);
 }
 
+function isDay(day) {
+  return daysOfWeek.includes(day);
+}
+
+function getAnimalSchedule(name) {
+  const { availability } = species.find((specie) => specie.name === name);
+  return availability;
+}
+
+function formatOfficeHour({ open, close }) {
+  return `Open from ${open}am until ${close}pm`;
+}
+
+function getDaySchedule(day) {
+  const result = {};
+  let exhibition;
+  let officeHour;
+
+  if (day === 'Monday') {
+    exhibition = 'The zoo will be closed!';
+    officeHour = 'CLOSED';
+  } else {
+    officeHour = formatOfficeHour(hours[day]);
+    exhibition = species
+      .filter((specie) => specie.availability.includes(day))
+      .map((specie) => specie.name);
+  }
+
+  result[day] = { officeHour, exhibition };
+
+  return result;
+}
+
+function getFullSchedule() {
+  let result = {};
+  daysOfWeek.forEach((day) => {
+    result = Object.assign(getDaySchedule(day), result);
+  });
+
+  return result;
+}
+
+function getSchedule(scheduleTarget) {
+  if (isName(scheduleTarget)) {
+    return getAnimalSchedule(scheduleTarget);
+  }
+  if (isDay(scheduleTarget)) {
+    return getDaySchedule(scheduleTarget);
+  }
+  return getFullSchedule();
+}
 module.exports = getSchedule;
